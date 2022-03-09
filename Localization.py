307190ab37053faa6b1053819cb742a17fe8c559
Robbie <roby.1383@yahoo.com>
2022-03-09 17:51:56 +0100

    Base of localization done

--- Localization.py
+++ Localization.py
@@ -1,10 +1,33 @@
+import numpy as np
+
 class Localization:
 
-    def __init__(self, delta_t):
+    def __init__(self, delta_t, state):
+        self.state = state
+        self.covariance = np.dot(np.identity(3), 0.1)
         self.delta_t = delta_t
-        self.A = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-        self.B = []
-        self.C = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        self.A = np.identity(3)
+        self.C = np.identity(3)
+        self.R = np.dot(np.identity(3), 0.1)
+        self.Q = np.dot(np.identity(3), 0.1)
 
-    def kalmanFilter(self, state, covariance, action, observation):
-        pass
+    def kalmanFilter(self, action, observation):
+
+        # Prediction
+        B = [[self.delta_t * np.cos(self.state[2]), 0], [self.delta_t * np.sin(self.state[2]), 2], [0, self.delta_t]]
+        statePrediction = np.matmul(self.A, self.state) + np.matmul(B, action)
+        covariancePrediction = np.matmul(np.matmul(self.A, self.covariance), np.transpose(self.A)) + self.R
+
+        # Correction
+        aux = np.matmul(np.matmul(self.C, statePrediction), np.transpose(self.C)) + self.Q
+        K = np.matmul(np.matmul(covariancePrediction, np.transpose(self.C)), np.linalg.pinv(aux))
+        newState = statePrediction + np.matmul(K, (observation - np.matmul(self.C, statePrediction)))
+        newCovariance = np.matmul(np.identity(3) - np.matmul(K, self.C), covariancePrediction)
+
+        # Update
+        self.state = newState
+        self.covariance = newCovariance
+
+    def getObservationPose(self, x, y, theta):
+        return [x, y, theta]
+        # slide 20 in "19 ARS - Localization with Kalman Filter.pdf"
--- Robot.py
+++ Robot.py
@@ -1,76 +1,42 @@
-import math
 import random
-from copy import copy
-
 import numpy as np
-from pygame.math import Vector2
-
-def right_intersection(point, wall_point1, wall_point2, sensor_start, sensor_end):
-    x, y = point
-    right = (x - wall_point1[0]) * (x - wall_point2[0]) <= 0 and \
-            (y - wall_point1[1]) * (y - wall_point2[1]) <= 0 and \
-            (x - sensor_start[0]) * (sensor_end[0] - sensor_start[0]) >= 0 and \
-            (y - sensor_start[1]) * (sensor_end[1] - sensor_start[1]) >= 0
-    return right
 
 class Robot:
 
-    def __init__(self, outer_wall, size):
+    def __init__(self, wall, size):
         self.radius = int(size / 2)
-        self.x, self.y = self.initPosition(outer_wall)
+        self.x, self.y = self.initPosition(wall)
         self.frontX = self.x + self.radius
         self.frontY = self.y
-        self.Vl = 0
-        self.Vr = 0
+        self.v = 0
+        self.w = 0
         self.theta = 0
-        self.speed = 3
-        self.sensors, _ = self.distanceToSensors(outer_wall)
-        self.safe_distance = 2
-        self.stop = False
-        self.last_wall = []
+        self.speed = 1
 
-    def initPosition(self, outer_wall):
-        x = random.randint(outer_wall[0][0] + self.radius, outer_wall[2][0] - self.radius)
-        y = random.randint(outer_wall[0][1] + self.radius, outer_wall[2][1] - self.radius)
+    def initPosition(self, wall):
+        x = random.randint(wall[0][0] + self.radius, wall[2][0] - self.radius)
+        y = random.randint(wall[0][1] + self.radius, wall[2][1] - self.radius)
         return x, y
 
-    def move(self, movement, delta_t, outer_wall):
+    def move(self, movement, delta_t):
         # Check keys for movement
-        # movement = [w, s, o, l, x, t, g]
-
-        if movement[0] == 1 or movement[5] == 1:
-            self.Vl += self.speed
-        if movement[1] == 1 or movement[6] == 1:
-            self.Vl -= self.speed
-        if movement[2] == 1 or movement[5] == 1:
-            self.Vr += self.speed
-        if movement[3] == 1 or movement[6] == 1:
-            self.Vr -= self.speed
+        # movement = [w, s, a, d, x]
+        if movement[0] == 1:
+            self.v += self.speed
+        if movement[1] == 1:
+            self.v -= self.speed
+        if movement[2] == 1:
+            self.w += 0.01
+        if movement[3] == 1:
+            self.w -= 0.01
         if movement[4] == 1:
-            self.Vl = 0
-            self.Vr = 0
+            self.v = 0
+            self.w = 0
 
         # If it's moving
-        if self.Vr != 0 or self.Vl != 0:
-            # Make sure not to get a division by zero when velocities are the same
-            if self.Vr == self.Vl:
-                next_x = self.x + ((self.Vl + self.Vr) / 2) * np.cos(self.theta) * delta_t
-                next_y = self.y - ((self.Vl + self.Vr) / 2) * np.sin(self.theta) * delta_t
-                new_theta = self.theta + (self.Vr - self.Vl) / (2 * self.radius) * delta_t
-            else:
-                R = self.radius * (self.Vl + self.Vr) / (self.Vr - self.Vl)
-                w = (self.Vr - self.Vl) / (self.radius * 2)
-                # Compute ICC
-                ICC = [self.x - R * np.sin(-self.theta), self.y + R * np.cos(self.theta)]
-                result = np.transpose(np.matmul(
-                    np.array([[np.cos(w * delta_t), -np.sin(w * delta_t), 0],
-                              [np.sin(w * delta_t), np.cos(w * delta_t), 0],
-                              [0, 0, 1]]),
-                    np.transpose(np.array([self.x - ICC[0], self.y - ICC[1], self.theta]))) + np.array(
-                    [ICC[0], ICC[1], w * delta_t])).transpose()
-                next_x, next_y, new_theta = result[0], result[1], result[2]
-            # update  sensors
-            self.sensors, walls = self.distanceToSensors(outer_wall)
+        if self.v != 0 or self.w != 0:
+            result = [self.x, self.y, self.theta] + np.matmul([[delta_t * np.cos(self.theta), 0], [delta_t * np.sin(self.theta), 2], [0, delta_t]], [self.v, self.w])
+            next_x, next_y, new_theta = result[0], result[1], result[2]
 
             # Transfer results from the ICC computation
             self.x = next_x
@@ -78,8 +44,23 @@ class Robot:
             self.theta = new_theta
             self.frontX, self.frontY = self.rotate(self.theta, self.radius)
 
-        return self.Vl, self.Vr, np.round(np.degrees(self.theta), 2)
+        return self.v, self.w, np.round(np.degrees(self.theta), 2)
 
+    def rotate(self, angle, r):
+        # Rotate the robot at a certain angle from the x-axis
+        front_x = self.x + np.cos(angle) * r
+        front_y = self.y + np.sin(angle) * r
+        return front_x, front_y
+
+"""
+    def right_intersection(point, wall_point1, wall_point2, sensor_start, sensor_end):
+        x, y = point
+        right = (x - wall_point1[0]) * (x - wall_point2[0]) <= 0 and \
+                (y - wall_point1[1]) * (y - wall_point2[1]) <= 0 and \
+                (x - sensor_start[0]) * (sensor_end[0] - sensor_start[0]) >= 0 and \
+                (y - sensor_start[1]) * (sensor_end[1] - sensor_start[1]) >= 0
+        return right
+        
     def distanceToSensors(self, outer_wall):
         dist = []
         walls = []
@@ -90,12 +71,6 @@ class Robot:
             walls.append(wall_out)
             angle += math.pi / 6
         return dist, walls
-
-    def rotate(self, angle, r):
-        # Rotate the robot at a certain angle from the x-axis
-        front_x = self.x + np.cos(angle) * r
-        front_y = self.y + np.sin(-angle) * r
-        return front_x, front_y
 
     def distance(self, wall, angle):
         dist = []
@@ -136,3 +111,4 @@ class Robot:
         else:
             min_dist_out_wall = 1500
         return min_dist_out_wall, wall
+"""
